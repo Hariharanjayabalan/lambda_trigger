08b0e5261fbd0ab265e300660caa4c3aacd96fcb
Hariharan Jayabalan <hjayabalan10@massmutual.com>
2022-08-27 16:50:59 -0400

    Uploaded

--- bulid.sh
+++ bulid.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo 'Hari testing'
-declare -a folder_name=('github_archive')
+#declare -a folder_name=('github_archive')
+folder_name='github_archive'
 rm -rf lambda_zip
 rm -rf comman_zip
 mkdir lambda_zip
