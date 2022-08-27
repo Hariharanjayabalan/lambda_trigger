07bd6a13a07d9b7a93a13ef6c790db107038c888
Hariharan Jayabalan <hjayabalan10@massmutual.com>
2022-08-27 16:19:59 -0400

    Updated zip

---
+++ bulid.sh
@@ -0,0 +1,13 @@
+declare -a folder_name=('github_archive')
+rm -rf lambda_zip
+rm -rf comman_zip
+mkdir lambda_zip
+mkdir comman_zip
+pip install -r requirements.txt -t comman_zip
+for pkg in ${folder_name[@]}
+do 
+echo $pkg
+zip -rq lambda_zip/upload.zip $pkg
+cd comman_zip
+zip -rq ../lambda_zip/upload.zip .
+done
